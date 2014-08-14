9fe2b77b148bf9b399d670b68af2d468797122a7
Ian Macalinao <me@ian.pw>
2014-08-14 11:40:28 -0500

    Implement

--- functionize.js
+++ functionize.js
@@ -1,2 +1,24 @@
-module.exports = function () {
-};
+'use strict';
+
+var _ = require('lodash');
+
+function functionize(object, fn) {
+  var ret = _.bind(fn, object);
+
+  // Bind all properties
+  _.forIn(object, function(value, key) {
+    if (typeof value === 'function') {
+      ret[key] = _.bind(value, object);
+    } else {
+      ret[key] = value;
+    }
+  });
+
+  return ret;
+}
+
+if (typeof window === 'undefined' && module) {
+  module.exports = functionize;
+} else if (window) {
+  window.functionize = functionize;
+}
--- test.js
+++ test.js
@@ -1,5 +1,25 @@
+'use strict';
+
 var expect = require('chai').expect;
-var functionize = require('..');
+var functionize = require('./functionize');
 
 describe('functionize', function() {
+  it('should follow the example', function() {
+    var object = {
+      counter: 1,
+      add: function(amt) {
+        return this.counter += amt;
+      }
+    };
+
+    // The normal way
+    expect(object.counter).to.equal(1);
+    expect(object.add(9)).to.equal(10);
+
+    // The functionized way
+    object = functionize(object, object.add);
+    expect(object.counter).to.equal(10);
+    expect(object(9)).to.equal(19);
+    expect(object.add(1)).to.equal(20);
+  });
 });
